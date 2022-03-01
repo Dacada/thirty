0ae3d67bc4fef2ded05c55ac28fc62b9979b33ed
David Carrera Casado <mail@davidcarreracasado.cat>
2022-03-01 13:55:27 +0100

    remove spurious comments

--- src/transform.c
+++ src/transform.c
@@ -46,25 +46,16 @@ void transform_set(struct transform *trans, vec3s value) {
         trans->model.col[3].z = value.z;
 }
 
-/*
- * Set X component of translation of model matrix to given value.
- */
 void transform_setX(struct transform *trans, float value) {
         assert(trans->base.type == COMPONENT_TRANSFORM);
         trans->model.col[3].x = value;
 }
 
-/*
- * Set Y component of translation of model matrix to given value.
- */
 void transform_setY(struct transform *trans, float value) {
         assert(trans->base.type == COMPONENT_TRANSFORM);
         trans->model.col[3].y = value;
 }
 
-/*
- * Set Z component of translation of model matrix to given value.
- */
 void transform_setZ(struct transform *trans, float value) {
         assert(trans->base.type == COMPONENT_TRANSFORM);
         trans->model.col[3].z = value;
